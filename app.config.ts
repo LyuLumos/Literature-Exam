cf7908004b6f61a757cb30041f3a30621d7b660c
LyuLumos <3208413453@qq.com>
2023-09-26 01:07:39 +0800

    init commit

---
+++ app.config.ts
@@ -0,0 +1,32 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Literature Exam',
+    description: 'Literature Exam of CUC.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      github: 'lyulumos/Literature-Exam',
+    },
+    github: {
+      dir: 'content',
+      branch: 'main',
+      repo: 'Literature-Exam',
+      owner: 'lyulumos',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
